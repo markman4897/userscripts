36a267478da2d40dc63cc04d2d5c3cbf3d8f2f2f
Mark Lisicic <lisicic.mark@gmail.com>
2019-09-24 14:44:38 +0200

    Versions: = 1.3 - makes alert when done (so you know its done even if nothing is marked) - migrating style to GM_addStyle tag

--- google_keep_duplicates.user.js
+++ google_keep_duplicates.user.js
@@ -2,11 +2,15 @@
 // @name        Google Keep duplicates
 // @description Adds a way to search for duplicate lines in google keep. (doesn't work with tick boxes)
 // @include     *keep.google.com*
+// @grant       GM_addStyle
 // @version     1.2
 // ==/UserScript==
 
 /*
 Versions:
+= 1.3
+- makes alert when done (so you know its done even if nothing is marked)
+- migrating style to GM_addStyle tag
 = 1.2
 - made it case not sensitive
 = 1.1
@@ -16,16 +20,65 @@ Versions:
 - button colors the identical lines (ignores "- ")
 */
 
+GM_addStyle(`
+#dupButton {
+position:fixed;
+right:50px;
+bottom:50px;
+z-index:9999;
+background-color: #202124;
+border-style: solid;
+border-width: 1px;
+border-color: #5f6368;
+color: #e8eaed;
+padding: 15px 32px;
+text-align: center;
+margin: 4px 2px;
+}
+
+#toast{
+position:fixed;
+right:200px;bottom:50px;
+z-index:9999;
+background-color: #202124;
+border-style: solid;
+border-width: 1px;
+border-color: #5f6368;
+color: #e8eaed;
+padding: 15px 32px;
+text-align: center;
+margin: 4px 2px;
+visibility: hidden;
+}
+
+#toast.show{
+visibility: visible;
+}
+`);
+
+// defining custom toast message
+var toast = document.createElement("div");
+//toast.appendChild(document.createTextNode("Done."));
+toast.id = "toast";
+document.body.appendChild(toast);
+
 // defining button attributes
 var btn = document.createElement("BUTTON");
 btn.innerHTML = "DUP";
-btn.style = "position:fixed;right:50px;bottom:50px;z-index:9999;background-color: #202124;border-style: solid;border-width: 1px;border-color: #5f6368;color: #e8eaed;padding: 15px 32px;text-align: center;margin: 4px 2px;";
 btn.id = "dupButton";
 // actually adding button into dom
 document.body.appendChild(btn);
 
 // adds event listener so that the function gets executed when the button is pressed
 document.getElementById ("dupButton").addEventListener("click", duplicates);
+
+// function to display alert
+function launch_toast(message) {
+    var x = document.getElementById("toast")
+    x.innerHTML = message;
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 1500);
+};
 
 // function to be executed on click of the button
 function duplicates() {
@@ -35,7 +88,7 @@ function duplicates() {
   // splits for later use
   var arr = dupes_field.split("<br>");
   // makes everything lower case (just in case), splits by "- " and joins back without so we get rid of it, splits by "<br>" so we have one line per array element, sorts so duplicates are next to each other (for easier detection later)
-  out = dupes_field.toLowerCase().split("- ").join("").split("<br>").sort();
+  var out = dupes_field.toLowerCase().split("- ").join("").split("<br>").sort();
 
   var dupes = [];
   // detects duplicates and puts them in array (dupes)
@@ -43,10 +96,10 @@ function duplicates() {
     if (out[i + 1] == out[i]) {
         dupes.push(out[i]);
     }
-	}
+  }
 
   // prepares new innerHTML for the original div
-  for (var i = 0; i < arr.length; i++) {
+  for (i = 0; i < arr.length; i++) {
     for (var j = 0; j < dupes.length; j++) {
       if (arr[i].toLowerCase().replace("- ","") == dupes[j]) {
         arr[i] = "<mark>"+arr[i]+"</mark>";
@@ -55,7 +108,12 @@ function duplicates() {
 	}
 
   // joining back the colorised things
-  result = arr.join("<br>");
+  var result = arr.join("<br>");
   // writing them to dom
   document.getElementsByClassName("IZ65Hb-s2gQvd")[document.getElementsByClassName("IZ65Hb-s2gQvd").length -1].querySelectorAll("*")[5].innerHTML = result;
-}
+  if(dupes.length == 0) {
+    launch_toast("No duplicated.");
+  } else {
+    launch_toast("Done.");
+  };
+};
